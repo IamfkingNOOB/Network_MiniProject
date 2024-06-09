63c8f18d1c78c590c9cd4ab70cf18e9b799911b9
IamfkingNOOB <iamfkingnoob@gmail.com>
2024-06-09 23:10:29 +0900

    【Feat】 Create Script for Chatting Panel

---
+++ Assets/Scripts/ChattingUI.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChattingUI : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI text_ChatText;
+    [SerializeField] private Scrollbar scrollbar_ChatPanel;
+    [SerializeField] private TMP_InputField inputField_InputMessage;
+    [SerializeField] private Button button_Send;
+
+
+}
